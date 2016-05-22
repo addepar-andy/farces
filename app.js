86f587e59046ea57b505020d27eb84ac2724338b
Andy Lei <andy@addepar.com>
2016-05-22 17:42:23 -0400

    admins can add and edit faces

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 (function() {
   let fire = new Firebase("https://addefarces.firebaseio.com/");
+  let currentUser = null;
+
+  function hide(node) {
+    node.setAttribute('style', 'display: none');
+  }
+
+  function show(node) {
+    node.setAttribute('style', '');
+  }
 
   function cloneFromTemplate(cls) {
     return document.getElementById('templates')
@@ -9,12 +18,23 @@
 
   function afterLogin(error, authData) {
     if (error) {
+      console.log(authData);
       alert('could not log you in');
+    } else {
+      //TODO: only save email if user doesn't already have email
+      fire.child('users')
+        .child(authData.uid)
+        .child('email')
+        .set(authData.google.email.toLowerCase());
     }
   }
 
   function login() {
     fire.authWithOAuthPopup("google", afterLogin, {scope: 'email'});
+  }
+
+  function logout() {
+    fire.unauth();
   }
 
   function newPerson() {
@@ -29,7 +49,7 @@
 
     document.getElementById('name-field').value = "";
     document.getElementById('image-field').value = "";
-    document.getElementById('new-person-form').setAttribute('style', 'display: none');
+    hide(document.getElementById('new-person-form'));
   }
 
   function renderPic(picsNode, picSnapshot) {
@@ -39,33 +59,84 @@
     picsNode.appendChild(picNode);
   }
 
+  function stopEditing(personNode, editorNode) {
+    personNode.removeChild(editorNode);
+    show(personNode.getElementsByClassName('edit')[0]);
+  }
+
+  function savePerson(personRef, personNode, editorNode, nameField, imageField) {
+    let person = {
+      name: nameField.value,
+      image: imageField.value
+    };
+    personRef.set(person, function() {
+      renderPerson1(personNode, person);
+      stopEditing(personNode, editorNode);
+    });
+  }
+
+  function editPerson(personRef, evt) {
+    personRef.once('value', function(personSnapshot) {
+      let person = personSnapshot.val();
+      let editBtn = evt.target;
+      let personNode = editBtn.parentNode;
+
+      let editorNode = cloneFromTemplate('person-editor');
+
+      let nameField = editorNode.getElementsByClassName('name')[0];
+      let imageField = editorNode.getElementsByClassName('image')[0];
+      let saveBtn = editorNode.getElementsByClassName('save')[0];
+      let cancelBtn = editorNode.getElementsByClassName('cancel')[0];
+
+      hide(editBtn);
+
+      nameField.value = person.name;
+      imageField.value = person.image;
+
+      saveBtn.addEventListener('click', (evt) =>
+          savePerson(personRef, personNode, editorNode, nameField, imageField));
+      cancelBtn.addEventListener('click', (evt) => stopEditing(personNode, editorNode));
+
+      personNode.appendChild(editorNode);
+    });
+  }
+
+  function renderPerson1(personNode, person) {
+    personNode.getElementsByTagName('h3')[0].innerText = person.name;
+    personNode.getElementsByClassName('primary-pic')[0].src = person.image;
+  }
+
   function renderPerson(personSnapshot) {
     let person = personSnapshot.val();
 
     let personNode = cloneFromTemplate('person');
-    personNode.getElementsByTagName('h3')[0].innerText = person.name;
-    personNode.getElementsByClassName('primary-pic')[0].src = person.image;
+    renderPerson1(personNode, person);
 
+    if (currentUser && currentUser.admin) {
+      let editBtn = personNode.getElementsByClassName('edit')[0];
+      editBtn.addEventListener('click', (evt) => editPerson(personSnapshot.ref(), evt));
+    }
+
+    document.getElementById('people').appendChild(personNode);
+
+    /*
     let addPicButton = personNode.getElementsByTagName('button')[0];
     let addPicInput = personNode.getElementsByTagName('input')[0];
     addPicButton.addEventListener('click', (evt) =>
       addPic(personSnapshot.ref(), addPicInput.value)
     );
 
-    //let toggleNode = document.createElement('button');
-    //toggleNode.setAttribute('onclick', 'toggleImages(this);');
-    //toggleNode.textContent = 'toggle'
-    //personNode.appendChild(toggleNode);
-
-    document.getElementById('people').appendChild(personNode);
+    let toggleNode = document.createElement('button');
+    toggleNode.setAttribute('onclick', 'toggleImages(this);');
+    toggleNode.textContent = 'toggle'
+    personNode.appendChild(toggleNode);
 
     let picsNode = personNode.getElementsByClassName('pics')[0];
     personSnapshot.ref()
       .child('pics')
       .orderByChild('negvotes')
       .on('child_added', (data) => renderPic(picsNode, data));
-
-    console.log(person);
+    */
   }
 
   function addPic(personRef, picUrl) {
@@ -78,10 +149,44 @@
     fire.child('people').on('child_added', renderPerson);
   }
 
+  function renderAdminStuff() {
+    if (currentUser && currentUser.admin) {
+      document.getElementById('new-person').setAttribute('style', '');
+    }
+  }
+
+  function renderLogout() {
+    hide(document.getElementById('login'));
+    show(document.getElementById('logout'));
+  }
+
+  function renderLogin() {
+    show(document.getElementById('login'));
+    hide(document.getElementById('logout'));
+    hide(document.getElementById('new-person'));
+    let peopleNode = document.getElementById('people');
+    while(peopleNode.firstChild) {
+      peopleNode.removeChild(peopleNode.firstChild);
+    }
+  }
+
   function init() {
-    renderPeople();
+    fire.onAuth((auth) => {
+      if (auth && auth.uid) {
+        fire.child('users').child(auth.uid).once('value', (userSnapshot) => {
+          currentUser = userSnapshot.val();
+          renderAdminStuff();
+        });
+        renderPeople();
+        renderLogout();
+      } else {
+        renderLogin();
+      }
+    });
 
     document.getElementById('login').addEventListener('click', login);
+
+    document.getElementById('logout').addEventListener('click', logout);
 
     document.getElementById('new-person').addEventListener('click', newPerson);
 
@@ -90,5 +195,11 @@
   init();
 })();
 
-
+// 1. adding / editing / deleting images as admin
+// 2. display images and names
+// 3. user added images (make sure its an imgur link)
+// 4. display user added images
+// 5. upvote user added images
+// 6. order user images by upvotes
+// 7. nice imgur ui
 
